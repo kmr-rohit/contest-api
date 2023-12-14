48775db005285593b11a64bdd92d94f4976e9d1d
kmr-rohit <rk972006@student.nitw.ac.in>
2023-12-14 17:35:13 +0530

    Change in Files

--- contest.js
+++ contest.js
@@ -1,18 +1,13 @@
+// contest.js
 const axios = require('axios');
 
-module.exports = async (req, res) => {
+exports.contest = async (req, res, next) => {
     try {
-      const apiUrl = 'https://clist.by/api/v4/json/contest/?upcoming=true&end_time__during=1728000&duration__gt=5400&duration__lt=10800&username=kmrrohit&api_key=b6bee0d321166ca219f3c2d83da9626a14043d93';
-      const response = await axios.get(apiUrl);
-      res.json(response.data);
+        const apiUrl = 'https://clist.by/api/v4/json/contest/?upcoming=true&end_time__during=1728000&duration__gt=5400&duration__lt=10800&username=kmrrohit&api_key=b6bee0d321166ca219f3c2d83da9626a14043d93';
+        const response = await axios.get(apiUrl);
+        req.contestData = response.data; // attach the data to the request object
+        next(); // proceed to the next middleware
     } catch (error) {
-      if (error instanceof AggregateError) {
-        for (const individualError of error.errors) {
-          console.error(individualError);
-        }
-      } else {
-        console.error(error);
-      }
-      res.status(500).json({ error: error.toString() });
+        next(error); // if there's an error, pass it to the error handling middleware
     }
 };
--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+// index.js
 const express = require('express')
 const app = express();
 const cors = require('cors')
-const contest = require('./contest'); // import the contest module
 
 app.use(cors({
     origin: '*'
 }))
+let contest = require('./contest');
 
-app.get('/', contest); // use the contest module as a middleware for your route
+
+app.get('/', (req,res) => {
+    res.send(`<b>API URL:</b>/<b style="color:crimson;">yourLeetcodeUsername</b>`)
+});
+
+app.get('/contest', contest.contest, (req, res) => {
+    res.json(req.contestData); // send the data as a JSON response
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
